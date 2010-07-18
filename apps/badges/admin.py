f7132c1e863fd95acd6a115bad94df90d29c94c9
Leslie Michael Orchard <l.m.orchard@pobox.com>
2010-07-18 17:03:48 -0400

    Added BadgeAwardee to admin panel

--- apps/badges/admin.py
+++ apps/badges/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from badger.apps.badges.models import Badge, BadgeNomination, BadgeAward
+from badger.apps.badges.models import Badge, BadgeNomination, BadgeAward, BadgeAwardee
 
 admin.site.register(Badge)
 admin.site.register(BadgeNomination)
 admin.site.register(BadgeAward)
+admin.site.register(BadgeAwardee)
