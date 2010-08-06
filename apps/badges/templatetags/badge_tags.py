bb6f321167d3d0c9c5012db65124bb3c3638bf2c
Leslie Michael Orchard <l.m.orchard@pobox.com>
2010-08-06 13:55:15 -0400

    Hide hidden awards from recent list on homepage

--- apps/badges/templatetags/badge_tags.py
+++ apps/badges/templatetags/badge_tags.py
@@ -92,7 +92,7 @@ class RecentBadgeAwardsNode(template.Node):
     
     def render(self, context):
         context[self.as_var] = BadgeAward.objects\
-                .filter(claimed=True).order_by('-updated_at')[:15]
+                .filter(claimed=True).exclude(hidden=True).order_by('-updated_at')[:15]
         # TODO: Is the select_related() necessary if we're using fragment
         # caching in the template? Produces a mighty complicated query.
         #context[self.as_var] = BadgeAward.objects.select_related()\
