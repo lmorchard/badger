c50ec1cfbd89fd2402273f2a0299eeb9762d1c2f
Leslie Michael Orchard <l.m.orchard@pobox.com>
2010-10-02 00:20:26 -0400

    Adding BADGE_DEFAULT_SIZE

--- apps/badges/__init__.py
+++ apps/badges/__init__.py
@@ -11,6 +11,7 @@ AUTO_GENERATE_BADGE_SIZES = getattr(settings, 'AUTO_GENERATE_BADGE_SIZES',
         (256,80,32,))
 BADGE_RESIZE_METHOD = getattr(settings, 'BADGE_RESIZE_METHOD', Image.ANTIALIAS)
 BADGE_STORAGE_DIR = getattr(settings, 'BADGE_STORAGE_DIR', 'badges')
+BADGE_DEFAULT_SIZE = 256
 BADGE_DEFAULT_URL = getattr(settings, 'BADGE_DEFAULT_URL', 
     settings.STATIC_URL + 'badges/img/default-badge.jpg')
 
